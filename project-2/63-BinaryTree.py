1eba468a2f63abd969ba83eec37fa018c0cf8e15
JROB296 <90108590+JROB296@users.noreply.github.com>
2021-10-17 21:16:39 -0500

    Add files via upload

---
+++ project-2/63-BinaryTree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data):
+        self.left_child = None
+        self.right_child = None
+        self.data = data
+
+# NODE CREATION
+i = 1
+while i <= 63 // 2:
+    child_nodes = []
+    parent_nodes = []
+    root = Node(i)
+    root.left_child = root.data * 2
+    root.right_child = (root.data * 2) + 1
+    parent_nodes.append(root.data)
+    child_nodes.extend([root.left_child, root.right_child])
+    i += 1
+
+    print("Parent Shelf: " + str(parent_nodes) + "\t Child Shelves: " + str(child_nodes))
